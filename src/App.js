caf718c3f66082112dc198b0dcb0d63f9a83f984
someonempty <someonempty@gmail.com>
2023-03-14 00:04:24 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import './styles/App.css';
 
 function App() {
-  
-  const [likes, setLikes] = useState(0);
   const [value, setValue] = useState('Some text');
-
-  function increment() {
-    setLikes(likes + 1);
-  }
-
-  function decrement() {
-    setLikes(likes - 1);
-  }
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input type="text"
-       value={value}
-       onChange={event => setValue(event.target.value)}
-       />
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <PostItem/>
+      <PostItem/>
+      <PostItem/>
     </div>
   );
 }
